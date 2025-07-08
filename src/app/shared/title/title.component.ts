cf532f34cfb5974a943b3d18cf990c3d8fce1597
Jonathan <contactoanonimo.x@gmail.com>
2025-07-08 16:36:48 -0600

    control flow

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-title',
-  imports: [],
-  templateUrl: './title.component.html',
-  styleUrl: './title.component.css'
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1 class="text-3xl mb-5">{{ title }}</h1>
+  `
 })
 export class TitleComponent {
+
+  @Input({ required: true }) title!: string;
+  @Input({ transform: booleanAttribute }) withShadow:boolean = false;
 
 }
