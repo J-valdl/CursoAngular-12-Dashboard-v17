9d378483eaa7095539fb908dc1e9bd0f22318700
Jonathan <contactoanonimo.x@gmail.com>
2025-07-08 17:05:22 -0600

    Defer block

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
-  imports: [],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
   styles: ``
 })
 export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => `Change detection - ${ this.frameworkAsSignal().name }`
+  );
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+        //this.frameworkAsProperty.name = 'React';
+        this.frameworkAsSignal.update( value => {
+          value.name = 'React';
+
+          return {...value};
+        })
+
+        console.log('Hecho');
+    }, 3000);
+  }
 
 }
