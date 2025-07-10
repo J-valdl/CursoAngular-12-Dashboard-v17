7a6255ee0bc03ef2f72b3ceb2b6bc9d8d37c8a66
Jonathan <contactoanonimo.x@gmail.com>
2025-07-10 10:11:32 -0600

    Defer

--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   imports: [CommonModule],
-  template: `<p>Hola Mundo!!</p>`,
-  styles: ``,
+  template: `<section [ngClass]="['w-full', cssClass ]">
+    <ng-content />
+  </section> `,
 })
 export class HeavyLoadersFastComponent {
+  @Input({ required: true }) cssClass!: string;
 
+  constructor() {
+    console.log('HeavyLoader Fast creado');
+  }
 }
